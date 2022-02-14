edb28f556eb4e92c9c8220b47f99119a3922fc98
VedGadge <vedgadge1906@gmail.com>
2022-02-14 21:12:17 +0530

    first commit

---
+++ Web3.0/client/src/App.jsx
@@ -0,0 +1,18 @@
+ import { Navbar, Welcome, Footer, Services, Transcations } from './components'
+
+const App = () =>  {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transcations/>
+      <Footer/> 
+
+    </div>
+  )
+}
+
+export default App
