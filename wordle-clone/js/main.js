9341cdbed7928d0a8d5656f90dd3528c6350ceb6
VedGadge <vedgadge1906@gmail.com>
2022-02-14 21:05:17 +0530

    first commit

---
+++ wordle-clone/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",() =>{
+
+    function createSqaures(){
+        const gameBoard =document.getElementById("board")
+
+        for (let index = 0; index < 30; index++) {
+            let;
+            
+        }
+    }
+})
